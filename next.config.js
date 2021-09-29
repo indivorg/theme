056eff87fce7f1ec5010ebc03c297ca191e397ec
Simen A. W. Olsen <so@bjerk.io>
2021-09-29 13:10:19 +0200

    chore: fix demo website

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ });
 module.exports = withMDX({
   pageExtensions: ['ts', 'tsx', 'mdx'],
-  // assetPrefix: isProd ? '' : ''
+  assetPrefix: isProd ? '/theme' : ''
 });
